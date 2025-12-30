d6e98b5c2b3a81214fffd8d8b3ac6d4e10f4efbd
umair <eeumairali@gmail.com>
2025-12-30 15:52:33 +0500

    updaetd but issue

--- S3 Games to understand/memory_game_gui/main.py
+++ S3 Games to understand/memory_game_gui/main.py
@@ -2,6 +2,7 @@ import turtle
 import os
 import random
 from card import Card
+import time
 
 # file_paths = "/Users/tarineesae-lee/Desktop/python-1/S3 Games to understand/memory_game_gui/images"
 file_paths = os.path.join("S3 Games to understand", "memory_game_gui", "images") # in macos path is S3 Games to understand/memory_game_gui/images and in windows path is S3 Games to understand\memory_game_gui\images
@@ -42,13 +43,53 @@ for each in allfiles_path2x:
     all_cards.append(card)
     
 screen.update()
+click_number=1
+
+visible_cards = []
+selected_card = []
+
+
 def karju(x, y):
-    print("screen clicked at", x, y)
-    for card in all_cards:
-        if abs(card.back.xcor() - x) < 50 and abs(card.back.ycor() - y) < 70:
-            print("card clicked:", card.image_file)
-            card.show_front()
-    screen.update()
+    global click_number
+    if click_number==1:
+        # when first time clicking the screen
+        print("screen clicked at", x, y)
+        for card in all_cards:
+            if abs(card.back.xcor() - x) < 50 and abs(card.back.ycor() - y) < 70:
+                print("card clicked:", card.image_file)
+                card.show_front()
+                selected_card.append(card)
+        click_number=click_number+1
+        screen.update()
+
+
+    elif click_number==2:
+        # when second time clicking the screen
+        print("screen clicked at", x, y)
+        for card in all_cards:
+            if abs(card.back.xcor() - x) < 50 and abs(card.back.ycor() - y) < 70:
+                print("card clicked:", card.image_file)
+                card.show_front()
+                selected_card.append(card)
+        
+        # check if two selected cards are same
+        if selected_card[0].image_file == selected_card[1].image_file:
+            print("It's a match!")
+            visible_cards.append(selected_card[0])
+            visible_cards.append(selected_card[1])
+        else:
+            print("Not a match.")
+            # hide the cards again after short delay
+            turtle.time.sleep(1)
+            for card in selected_card:
+                if card not in visible_cards:
+                    card.show_back()
+
+        screen.update()
+        time.sleep(0.5)
+        selected_card.clear()
+        click_number=1
+
 
 
 screen.onclick(karju)
